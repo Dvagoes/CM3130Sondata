416c650f70c51ce47a2e60f4308715e73dc09938
Kylzo01 <43371121+Kylzo01@users.noreply.github.com>
2020-11-10 00:16:44 +0000

    settin up

---
+++ assets/scripts/script.js
@@ -0,0 +1,28 @@
+window.addEventListener('load', function () {
+    
+  console.log("Javascript is working")
+
+
+
+var wavesurfer = WaveSurfer.create({
+
+    container: '#visual_output',
+    waveColor: 'red',
+    progressColor: 'purple',
+
+    plugins: [
+        window.WaveSurfer.spectrogram.create({
+            wavesurfer: wavesurfer,
+            container: "#wave-spectrogram",
+        })
+    ]
+});
+console.log("Pog")
+//wavesurfer.load('../assets/Audio/birds.mp3');
+
+wavesurfer.load('../assets/Audio/birds.mp3')
+
+
+
+
+})
