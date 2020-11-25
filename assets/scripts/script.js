28b1b3d24585595b6ff82e1bfbbe4d3a9484ea1b
Fynises <fraser_irvine@outlook.com>
2020-11-25 17:17:48 +0000

    general code cleanup

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,21 +1,20 @@
-//currently early implementation subject to changes later
+/**
+ * code for handling file upload and audio image downloading
+ */
 
-//to get file drag and drop working. you need to drag the file over "Choose an Audio File" on the webpage
-
+//imports
 var router = express.Router();
-
 var ejs = require('ejs');
-
 var html2canvas = require('html2canvas');
 var FileSaver = require('file-saver');
 
-//following code is taken from https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/File_drag_and_drop
-
+//The following drag and drop code is taken from https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/File_drag_and_drop
 
 function dropHandler(ev) {
+   
     console.log('File(s) dropped');
 
-    ev.preventDefault()
+    ev.preventDefault();
     let file;
     if (ev.dataTransfer.items) {
         for (var i = 0; i < ev.dataTransfer.items.length; i++) {
@@ -27,6 +26,20 @@ function dropHandler(ev) {
                 fileExtension = fName.split('.').pop();
                 console.log("test: " + fileExtension);
 
+                //simple if statement to check if file dropped is mp3
+                if (fileExtension != 'mp3') {
+                    alert("Invalid file type detected. \n \n please upload a .mp3 file");
+                    return;
+                }
+                
+                /**
+                 * commented out block below is used to validate filetype for drag and drop.
+                 * 
+                 * it has since been replaced with the code above due to the lack of
+                 * filetype conversion functionality post file upload.
+                 */
+
+                /*
                 switch(fileExtension) {
                     case 'mp3':
                         break;
@@ -44,41 +57,51 @@ function dropHandler(ev) {
                         alert("Invalid file type detected. \n please upload an audio file");
                         return;
                 }
+                */
 
+                //test console.log
                 // console.log('...file [' + i + '].name = ' + file.name);
                 fileProcess(file);
             }
         }
     } else {
         for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+            // test console.log
             // console.log('...file [' + i + '].name = ' + file.name);
-            // fileProcess(file);
             console.log("else statement called");
         }
     }
 }
 
+/**
+ * dragOverHandler() function is currently deprecated
+ * 
+ * it was used to identify if a file is above the drag and drop area.
+ */
+
 function dragOverHandler(ev) {
-    // console.log('File(s) in drop zone');
+    //test console log
+    //console.log('File(s) in drop zone');
 
     ev.preventDefault();
 }
 
-function formOnSubmit(ev) {
+/**
+ * formOnSubmit() gets called when the user attempts to upload a file manually
+ * by pressing the button
+ */
 
+function formOnSubmit(ev) {
     ev.preventDefault();
     console.log(ev);
-    // let el = document.getElementById("audio_upload");
     let file = ev.target.files[0];
-    //console.log(el.files);
-    //console.log("file upload test" + el.name);
     fileProcess(file);
 }
 
 /**
- * fileProcess takes the "file" parameter to display file details in json format.
- * this will be used as a staging point for getting files uploaded temporarily.
- * currently it only gets the file details printed to console in json format.
+ * fileProcess takes the details of the uploaded file and converts the file into a String Datastream
+ * 
+ * this datastream then gets sent to the backend where it will be reconstructed as an audio file and saved
  */
 
 function fileProcess(file) {
@@ -105,7 +128,7 @@ function fileProcess(file) {
         body: base64,
         redirect: 'follow'
         };
-        console.log("test2")
+        console.log("test Arraybuffer")
         fetch("/", requestOptions)
         .then(response => response.text())
         .then(response => createSpectrogram(file))
@@ -114,9 +137,18 @@ function fileProcess(file) {
     })
 }
 
+/**
+ * createSpectrogram() function prepares to create a wavesurfer-js waveform and spectrogram.
+ * 
+ * this function reads from the sound file that had been sent to the backend.
+ * 
+ * ideally this function should only read the file from the backend to avoid possible problems with limitations on the front end.
+ * 
+ * however this function can read the file from the front end.
+ * hence why there is a file parameter.
+ */
+
 function createSpectrogram(file) {
-    
-    //var alreadyRunning = false;
     
         var wavesurfer = WaveSurfer.create({
             
@@ -132,13 +164,24 @@ function createSpectrogram(file) {
                 })
             ]
         });
-        console.log("test3")
+
+        //test console.log
+        console.log("test Create Spectrogram function called");
+
+        //change code below to wavesurfer.loadBlob(file);
+        //if loading from the backend causes issues
         wavesurfer.load('Audio/sound.mp3');
 
         wavesurfer.spectrogram;
         alreadyRunning = true;
-
 }
+
+/**
+ * downloadWaveform() function gets called from pressing the "download waveform" button on the website.
+ * 
+ * this function converts the div containing the waveform into a temporary canvas which is then converted into an image
+ * and is then downloaded.
+ */
 
 function downloadWaveform(ev) {
     ev.preventDefault();
@@ -156,6 +199,13 @@ function downloadWaveform(ev) {
     });
 }
 
+/**
+ * downloadSpectrogram() function gets called from pressing the "download spectrogram" button on the website.
+ * 
+ * this function converts the div containing the spectrogram into a temporary canvas which is then converted into an image
+ * and is then downloaded.
+ */
+
 function downloadSpectrogram(ev) {
     ev.preventDefault();
 
@@ -172,108 +222,12 @@ function downloadSpectrogram(ev) {
     });
 }
 
-
-
 /**
-function postmanTest(file){
-    
-    var reader = new FileReader();
-
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "text/plain");
-
-    reader.readAsArrayBuffer(file);
-
-    var raw = reader.result;
-
-    file.arrayBuffer().then(buffer => {
-        console.log(buffer); 
-
-        var base64 = window.btoa(
-            new Uint8Array(buffer)
-              .reduce((data, byte) => data + String.fromCharCode(byte), '')
-        );
-        console.log(base64);
-        var requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: base64,
-        redirect: 'follow'
-        };
-
-        // fetch("/", requestOptions)
-        // .then(response => response.text())
-        // .then(result => console.log(result))
-        // .catch(error => console.log('error', error));
-    })
-
-}
-*/
-//code below from previous attempts of implementing file uploading
-/**
-function fileUpload(file) {
-
-    var express = require('express');
-    var app = express();
-
-    app.post(null, upload.single('file'), function(req,res,next){
-
-    });
-
-    app.post('')
-
-
-    ============================================
-    
-    var uploads = multer({dest: './assets/Audio'});
-
-    router.post('/register', uploads.single(this.file), function(req,res,next){
-        if (req.file){
-            console.log('audio uploaded');
-        }
-    });
-
-    ===========================================
-
-    var storage = multer.diskStorage({
-        destination: function(req,file,callback){
-            callback(null, './assets/Audio');
-        },
-        filename:function(req,file,callback){
-            callback(null, file.fieldname);
-        }
-
-    })
-
-    var upload = multer({storage:storage});
-    app.get(null, function(req,res){
-        res.sendFile('./assets/Audio')
-    });
-    app.post(null, upload.single(this.file),function(req,res){
-        console.log(req.file);
-        console.log(req.file.path);
-    });
-
-
-    const multer = require("multer");
-    const multerConf = {
-        storage : multer.diskStorage({
-            destination : function(req, file, next) {
-                next(null, './assets/Audio');
-            },
-            filename: function(req,file,next){
-                const ext = file.mimetype.split('/'[1]);
-                next(null, audio + ext)
-                console.log(file);
-            }
-        })
-    }
-
-    router.post('/upload',multer(multerConf).single('audio'), function(req,res){
-        res.send('this is post route upload');
-    })
-
-}
-*/
-
-//herebedragons
+ * the reason why getting the image of the waveform and spectrogram is done as such
+ * is due to problems with using wavesurfer-js's exportImage() method.
+ * 
+ * the furthest i've gotten on getting an image from exportImage was a png with a small canvas
+ * filled with alpha.
+ * 
+ * documentation on wavesurfer-js's exportImage() method wasn't very detailed
+ */
